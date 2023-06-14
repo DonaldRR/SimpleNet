cc790dafff2cc0ca6c5989da81266ab5e851c272
DonaldRR <13822743137@163.com>
2023-06-14 14:20:48 +0800

    add copyright announcement

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# ------------------------------------------------------------------
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# ------------------------------------------------------------------
+# Modified by Yiming Zhou
+# ------------------------------------------------------------------
+
 import logging
 import os
 import sys
